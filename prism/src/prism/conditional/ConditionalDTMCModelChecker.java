a824a71f4ada984d2e8d09643cb7e819ae02bfa0
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2015-12-11 16:10:09 +0100

    imported patch symb-dtmc-quotient.patch
    # HG changeset patch
    # User Joachim Klein <klein@tcs.inf.tu-dresden.de>
    # Date 1449818494 -3600
    #      Fri Dec 11 08:21:34 2015 +0100
    # Node ID 8074bb367be7357beee3a1328d399e8f51df75f8
    # Parent  64478cfc964e77a07e0acf1fdd8479025b9c95c4
    imported patch symb-dtmc-quotient.patch

--- prism/src/prism/conditional/ConditionalDTMCModelChecker.java
+++ prism/src/prism/conditional/ConditionalDTMCModelChecker.java
@@ -3,16 +3,19 @@ package prism.conditional;
 import jdd.JDD;
 import jdd.JDDNode;
 import parser.ast.Expression;
+import parser.ast.ExpressionBinaryOp;
 import parser.ast.ExpressionConditional;
+import parser.ast.ExpressionProb;
+import parser.ast.RelOp;
 import prism.ModelChecker;
 import prism.ModelTransformation;
 import prism.Prism;
 import prism.PrismComponent;
 import prism.PrismException;
+import prism.PrismNotSupportedException;
 import prism.PrismSettings;
 import prism.ProbModel;
 import prism.ProbModelChecker;
-import prism.StateModelChecker;
 import prism.StateValues;
 
 public class ConditionalDTMCModelChecker extends ConditionalModelChecker<ProbModel> {
@@ -27,6 +30,17 @@ public class ConditionalDTMCModelChecker extends ConditionalModelChecker<ProbMod
 	@Override
 	public StateValues checkExpression(final ProbModel model, final ExpressionConditional expression, JDDNode statesOfInterest) throws PrismException {
 		final ConditionalTransformer<ProbModelChecker, ProbModel> transformer = selectModelTransformer(model, expression);
+		if (transformer == null) {
+			// try quotient as last resort
+			Expression quotientExpression = transformForQuotient(expression);
+			if (quotientExpression != null) {
+				prism.getLog().println("Checking quotient expression: "+quotientExpression);
+				return mc.checkExpression(quotientExpression, statesOfInterest);
+			} else {
+				throw new PrismNotSupportedException("Cannot model check conditional expression " + expression + " (with the current settings)");
+			}
+		}
+
 		final ModelTransformation<ProbModel, ProbModel> transformation = transformModel(transformer, model, expression, statesOfInterest);
 		final StateValues resultTransformed = checkExpressionTransformedModel(transformation, expression);
 
@@ -67,7 +81,7 @@ public class ConditionalDTMCModelChecker extends ConditionalModelChecker<ProbMod
 			return new MCLTLTransformer(mc, prism);
 		}
 
-		throw new PrismException("Cannot model check " + expression);
+		return null;
 	}
 
 	private StateValues checkExpressionTransformedModel(final ModelTransformation<ProbModel, ProbModel> transformation, final ExpressionConditional expression) throws PrismException {
@@ -82,6 +96,49 @@ public class ConditionalDTMCModelChecker extends ConditionalModelChecker<ProbMod
 		final StateValues result = mcTransformed.checkExpression(transformedExpression, JDD.Constant(1));
 		timer = System.currentTimeMillis() - timer;
 		prism.getLog().println("Time for property checking in transformed model: " + timer / 1000.0 + " seconds.");
+
+		return result;
+	}
+	
+	private Expression transformForQuotient(ExpressionConditional expr) throws PrismException {
+		if (!(expr.getObjective() instanceof ExpressionProb)) {
+			throw new PrismNotSupportedException("Can not transform expression with quotient method: "+expr);
+		}
+
+		ExpressionProb top = (ExpressionProb) expr.getObjective().deepCopy();
+		Expression bound = top.getBound();
+		RelOp relOp = top.getRelOp();
+		if (top.getBound() != null) {
+			// P bowtie bound
+			top.setBound(null);
+			top.setRelOp(RelOp.EQ);
+		}
+		ExpressionProb bottom = (ExpressionProb) top.deepCopy();
+		bottom.setExpression(expr.getCondition().deepCopy());
+		top.setExpression(Expression.And(Expression.Parenth(top.getExpression()),
+		                                 Expression.Parenth(bottom.getExpression().deepCopy())));
+		Expression result = new ExpressionBinaryOp(ExpressionBinaryOp.DIVIDE,
+		                                           top, bottom);
+		if (bound != null) {
+			int op = -1;
+			switch (relOp) {
+			case GEQ:
+				op = ExpressionBinaryOp.GE;
+				break;
+			case GT:
+				op = ExpressionBinaryOp.GT;
+				break;
+			case LEQ:
+				op = ExpressionBinaryOp.LE;
+				break;
+			case LT:
+				op = ExpressionBinaryOp.LT;
+				break;
+			default:
+				throw new PrismNotSupportedException("Unsupported comparison operator in "+expr);
+			}
+			result = new ExpressionBinaryOp(op, Expression.Parenth(result), bound.deepCopy());
+		}
 
 		return result;
 	}
