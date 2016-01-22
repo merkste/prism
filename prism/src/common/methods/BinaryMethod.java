383e329478863fca8ba5fb1dcfee3e6d53f68af1
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-22 15:58:22 +0100

    deprecate method wrappers

--- prism/src/common/methods/BinaryMethod.java
+++ prism/src/common/methods/BinaryMethod.java
@@ -3,6 +3,10 @@ package common.methods;
 import common.functions.PairMapping;
 import common.functions.TripleMapping;
 
+/**
+ * @deprecated
+ * Use J8 Functions instead.
+ */
 public interface BinaryMethod<Q, R, S, T> extends TripleMapping<Q, R, S, T>
 {
 	default PairMapping<R, S, T> on(final Q instance)
