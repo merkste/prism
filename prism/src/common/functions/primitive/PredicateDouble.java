b134fb97b0790ce0ade19655ed7a5ab93b2d913d
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-23 21:06:25 +0100

    let Predicate extend java.util.Predicate

--- prism/src/common/functions/primitive/PredicateDouble.java
+++ prism/src/common/functions/primitive/PredicateDouble.java
@@ -2,6 +2,7 @@ package common.functions.primitive;
 
 import common.functions.Predicate;
 
+// Cannot extend DoublePredicate due to negate() signature clash
 public interface PredicateDouble extends Predicate<Double>
 {
 	public boolean test(double element);
--- prism/src/common/functions/primitive/PredicateInteger.java
+++ prism/src/common/functions/primitive/PredicateInteger.java
@@ -2,6 +2,7 @@ package common.functions.primitive;
 
 import common.functions.Predicate;
 
+//Cannot extend IntPredicate due to negate() signature clash
 public interface PredicateInteger extends Predicate<Integer>
 {
 	public boolean test(int element);
