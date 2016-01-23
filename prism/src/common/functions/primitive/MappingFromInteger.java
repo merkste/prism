efbab259c361812aa657282da7c1e96c410a8c6f
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-23 21:03:07 +0100

    let Mapping extend Function

--- prism/src/common/functions/primitive/MappingFromInteger.java
+++ prism/src/common/functions/primitive/MappingFromInteger.java
@@ -1,8 +1,10 @@
 package common.functions.primitive;
 
+import java.util.function.IntFunction;
+
 import common.functions.Mapping;
 
-public interface MappingFromInteger<T> extends Mapping<Integer, T>
+public interface MappingFromInteger<T> extends Mapping<Integer, T>, IntFunction<T>
 {
 	public T apply(int element);
 }
