85cc402551183e306ab88737107c5c58a5585266
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-25 07:53:06 +0100

    move methods from AbstractMappingFromInteger to MappingFromInteger as default implementations

--- prism/src/common/functions/primitive/AbstractMappingFromInteger.java
+++ prism/src/common/functions/primitive/AbstractMappingFromInteger.java
@@ -4,20 +4,8 @@ import common.functions.AbstractMapping;
 
 public abstract class AbstractMappingFromInteger<T> extends AbstractMapping<Integer, T>implements MappingFromInteger<T>
 {
-	@Override
-	public T apply(final Integer element)
-	{
-		return apply(element.intValue());
-	}
-
 	public static <T> MappingFromInteger<T> constantFromInteger(final T value)
 	{
-		return new AbstractMappingFromInteger<T>() {
-			@Override
-			public T apply(final int element)
-			{
-				return value;
-			}
-		};
+		return MappingFromInteger.constantFromInteger(value);
 	}
 }
--- prism/src/common/functions/primitive/MappingFromInteger.java
+++ prism/src/common/functions/primitive/MappingFromInteger.java
@@ -7,4 +7,21 @@ import common.functions.Mapping;
 public interface MappingFromInteger<T> extends Mapping<Integer, T>, IntFunction<T>
 {
 	public T apply(int element);
+
+	@Override
+	default T apply(final Integer element)
+	{
+		return apply(element.intValue());
+	}
+
+	public static <T> MappingFromInteger<T> constantFromInteger(final T value)
+	{
+		return new MappingFromInteger<T>() {
+			@Override
+			public T apply(final int element)
+			{
+				return value;
+			}
+		};
+	}
 }
