e53c2cb720d3e698d06b7a74621d83815cd3e2a5
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-25 08:18:36 +0100

    move methods from AbstractPairMapping to PairMapping as default implementations

--- prism/src/common/functions/AbstractPairMapping.java
+++ prism/src/common/functions/AbstractPairMapping.java
@@ -2,27 +2,8 @@ package common.functions;
 
 public abstract class AbstractPairMapping<R, S, T> implements PairMapping<R, S, T>
 {
-	@Override
-	public Mapping<S, T> curry(final R element1)
-	{
-		return new AbstractMapping<S, T>()
-		{
-			@Override
-			public T apply(final S element2)
-			{
-				return AbstractPairMapping.this.apply(element1, element2);
-			}
-		};
-	}
-
 	public static <R, S, T> PairMapping<R, S, T> constant(final T value)
 	{
-		return new AbstractPairMapping<R, S, T>() {
-			@Override
-			public T apply(final R element1, final S element2)
-			{
-				return value;
-			}
-		};
+		return PairMapping.constant(value);
 	}
 }
