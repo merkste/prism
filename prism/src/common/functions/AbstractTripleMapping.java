0862c31cc623fb54c5793f52916854cb9393153a
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-25 08:33:32 +0100

    move methods from AbstractTripleMapping to TripleMapping as default implementations

--- prism/src/common/functions/AbstractTripleMapping.java
+++ prism/src/common/functions/AbstractTripleMapping.java
@@ -2,29 +2,4 @@ package common.functions;
 
 public abstract class AbstractTripleMapping<Q, R, S, T> implements TripleMapping<Q, R, S, T>
 {
-	@Override
-	public PairMapping<R, S, T> curry(final Q element1)
-	{
-		return new AbstractPairMapping<R, S, T>()
-		{
-			@Override
-			public T apply(final R element2, final S element3)
-			{
-				return AbstractTripleMapping.this.apply(element1, element2, element3);
-			}
-		};
-	}
-
-	@Override
-	public Mapping<S, T> curry(final Q element1, final R element2)
-	{
-		return new AbstractMapping<S, T>()
-		{
-			@Override
-			public T apply(final S element3)
-			{
-				return AbstractTripleMapping.this.apply(element1, element2, element3);
-			}
-		};
-	}
 }
--- prism/src/common/functions/TripleMapping.java
+++ prism/src/common/functions/TripleMapping.java
@@ -4,7 +4,27 @@ public interface TripleMapping<Q, R, S, T>
 {
 	public T apply(Q element1, R element2, S element3);
 
-	public PairMapping<R, S, T> curry(Q element1);
+	default PairMapping<R, S, T> curry(final Q element1)
+	{
+		return new PairMapping<R, S, T>()
+		{
+			@Override
+			public T apply(final R element2, final S element3)
+			{
+				return TripleMapping.this.apply(element1, element2, element3);
+			}
+		};
+	}
 
-	public Mapping<S, T> curry(Q element1, R element2);
+	default Mapping<S, T> curry(final Q element1, final R element2)
+	{
+		return new Mapping<S, T>()
+		{
+			@Override
+			public T apply(final S element3)
+			{
+				return TripleMapping.this.apply(element1, element2, element3);
+			}
+		};
+	}
 }
