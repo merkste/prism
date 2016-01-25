3773e7edd231ae3a0f0eca6d1d5e5cd69d350bfa
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-25 08:27:42 +0100

    move methods from AbstractPairPredicateInteger to PairPredicateInteger as default implementations

--- prism/src/common/functions/primitive/PairPredicateIntegerInteger.java
+++ prism/src/common/functions/primitive/PairPredicateIntegerInteger.java
@@ -4,8 +4,84 @@ import common.functions.PairPredicate;
 
 public interface PairPredicateIntegerInteger extends PairPredicate<Integer, Integer>
 {
-	public boolean test(int element1, int element2);
+	public abstract boolean test(final int element1, final int element2);
 
 	@Override
-	public PairPredicateIntegerInteger negate();
+	default boolean test(final Integer element1, final Integer element2)
+	{
+		return test(element1.intValue(), element2.intValue());
+	}
+
+	@Override
+	default PredicateInteger curry(final Integer element1)
+	{
+		return curry(element1.intValue());
+	}
+
+	default PredicateInteger curry(final int element1)
+	{
+		return new PredicateInteger()
+		{
+			@Override
+			public boolean test(final int element2)
+			{
+				return PairPredicateIntegerInteger.this.test(element1, element2);
+			}
+		};
+	}
+
+	@Override
+	default PairPredicateIntegerInteger negate()
+	{
+		return new PairPredicateIntegerInteger()
+		{
+			@Override
+			public final boolean test(final int element1, final int element2)
+			{
+				return !PairPredicateIntegerInteger.this.test(element1, element2);
+			}
+
+			@Override
+			public PairPredicateIntegerInteger negate()
+			{
+				return PairPredicateIntegerInteger.this;
+			}
+		};
+	}
+
+	default PairPredicateIntegerInteger and(final PairPredicateIntegerInteger predicate)
+	{
+		return new PairPredicateIntegerInteger()
+		{
+			@Override
+			public final boolean test(final int element1, final int element2)
+			{
+				return PairPredicateIntegerInteger.this.test(element1, element2) && predicate.test(element1, element2);
+			}
+		};
+	}
+
+	default PairPredicateIntegerInteger or(final PairPredicateIntegerInteger predicate)
+	{
+		return new PairPredicateIntegerInteger()
+		{
+			@Override
+			public final boolean test(final int element1, final int element2)
+			{
+				return PairPredicateIntegerInteger.this.test(element1, element2) || predicate.test(element1, element2);
+			}
+		};
+	}
+
+	default PairPredicateIntegerInteger implies(final PairPredicateIntegerInteger predicate)
+	{
+		return new PairPredicateIntegerInteger()
+		{
+			@Override
+			public final boolean test(final int element1, final int element2)
+			{
+				return (!PairPredicateIntegerInteger.this.test(element1, element2)) || predicate.test(element1, element2);
+			}
+		};
+	}
 }
