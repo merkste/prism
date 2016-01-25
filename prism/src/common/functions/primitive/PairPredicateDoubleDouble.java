a3b5906a5bcdf40ca75cd770ae7680fe47e9c691
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-25 08:25:22 +0100

    move methods from AbstractPairPredicateDouble to PairPredicateDouble as default implementations

--- prism/src/common/functions/primitive/PairPredicateDoubleDouble.java
+++ prism/src/common/functions/primitive/PairPredicateDoubleDouble.java
@@ -6,17 +6,95 @@ public interface PairPredicateDoubleDouble extends PairPredicate<Double, Double>
 {
 	public boolean test(double element1, double element2);
 
-	public PredicateDouble curry(double element1);
+	@Override
+	default boolean test(final Double element1, final Double element2)
+	{
+		return test(element1.doubleValue(), element2.doubleValue());
+	}
 
 	@Override
-	public PairPredicateDoubleDouble negate();
+	default PredicateDouble curry(final Double element1)
+	{
+		return curry(element1.doubleValue());
+	}
 
-	public PairPredicateDoubleDouble and(PairPredicateDoubleDouble predicate);
-
-	public PairPredicateDoubleDouble or(PairPredicateDoubleDouble predicate);
-
-	public PairPredicateDoubleDouble implies(PairPredicateDoubleDouble predicate);
+	default PredicateDouble curry(final double element1)
+	{
+		return new AbstractPredicateDouble()
+		{
+			@Override
+			public boolean test(final double element2)
+			{
+				return PairPredicateDoubleDouble.this.test(element1, element2);
+			}
+		};
+	}
 
 	@Override
-	public PairPredicateDoubleDouble inverse();
+	default PairPredicateDoubleDouble negate()
+	{
+		return new AbstractPairPredicateDoubleDouble()
+		{
+			@Override
+			public final boolean test(final double element1, final double element2)
+			{
+				return !PairPredicateDoubleDouble.this.test(element1, element2);
+			}
+
+			@Override
+			public PairPredicateDoubleDouble negate()
+			{
+				return PairPredicateDoubleDouble.this;
+			}
+		};
+	}
+
+	default PairPredicateDoubleDouble and(final PairPredicateDoubleDouble predicate)
+	{
+		return new PairPredicateDoubleDouble()
+		{
+			@Override
+			public final boolean test(final double element1, final double element2)
+			{
+				return PairPredicateDoubleDouble.this.test(element1, element2) && predicate.test(element1, element2);
+			}
+		};
+	}
+
+	default PairPredicateDoubleDouble or(final PairPredicateDoubleDouble predicate)
+	{
+		return new PairPredicateDoubleDouble()
+		{
+			@Override
+			public final boolean test(final double element1, final double element2)
+			{
+				return PairPredicateDoubleDouble.this.test(element1, element2) || predicate.test(element1, element2);
+			}
+		};
+	}
+
+	default PairPredicateDoubleDouble implies(final PairPredicateDoubleDouble predicate)
+	{
+		return new PairPredicateDoubleDouble()
+		{
+			@Override
+			public final boolean test(final double element1, final double element2)
+			{
+				return (!PairPredicateDoubleDouble.this.test(element1, element2)) || predicate.test(element1, element2);
+			}
+		};
+	}
+
+	@Override
+	default PairPredicateDoubleDouble inverse()
+	{
+		return new PairPredicateDoubleDouble()
+		{
+			@Override
+			public boolean test(double element1, double element2)
+			{
+				return PairPredicateDoubleDouble.this.test(element2, element1);
+			}
+		};
+	}
 }
