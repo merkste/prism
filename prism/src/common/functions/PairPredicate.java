1c3ff7155c06fbbed712ae04160786d541f0930c
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-01-25 08:21:59 +0100

    move methods from AbstractPairPredicate to PairPredicate as default implementations

--- prism/src/common/functions/PairPredicate.java
+++ prism/src/common/functions/PairPredicate.java
@@ -7,18 +7,90 @@ public interface PairPredicate<R, S> extends PairMapping<R, S, Boolean>, BiPredi
 	public boolean test(R element1, S element2);
 
 	@Override
-	public Predicate<S> curry(R element1);
+	default Boolean apply(final R element1, final S element2)
+	{
+		return test(element1, element2);
+	}
 
 	@Override
-	public PairPredicate<R, S> negate();
+	default Predicate<S> curry(final R element1)
+	{
+		return new Predicate<S>()
+		{
+			@Override
+			public boolean test(final S element2)
+			{
+				return PairPredicate.this.test(element1, element2);
+			}
+		};
+	}
 
 	@Override
-	public PairPredicate<R, S> and(BiPredicate<? super R, ? super S> predicate);
+	default PairPredicate<R, S> negate()
+	{
+		return new PairPredicate<R, S>()
+		{
+			@Override
+			public final boolean test(final R element1, final S element2)
+			{
+				return !PairPredicate.this.test(element1, element2);
+			}
+
+			@Override
+			public PairPredicate<R, S> negate()
+			{
+				return PairPredicate.this;
+			}
+		};
+	}
 
 	@Override
-	public PairPredicate<R, S> or(BiPredicate<? super R, ? super S> predicate);
+	default PairPredicate<R, S> and(BiPredicate<? super R, ? super S> predicate)
+	{
+		return new PairPredicate<R, S>()
+		{
+			@Override
+			public final boolean test(final R element1, final S element2)
+			{
+				return PairPredicate.this.test(element1, element2) && predicate.test(element1, element2);
+			}
+		};
+	}
 
-	public PairPredicate<R, S> implies(BiPredicate<? super R, ? super S> predicate);
+	@Override
+	default PairPredicate<R, S> or(BiPredicate<? super R, ? super S> predicate)
+	{
+		return new PairPredicate<R, S>()
+		{
+			@Override
+			public final boolean test(final R element1, final S element2)
+			{
+				return PairPredicate.this.test(element1, element2) || predicate.test(element1, element2);
+			}
+		};
+	}
 
-	public PairPredicate<S, R> inverse();
+	default PairPredicate<R, S> implies(BiPredicate<? super R, ? super S> predicate)
+	{
+		return new PairPredicate<R, S>()
+		{
+			@Override
+			public final boolean test(final R element1, final S element2)
+			{
+				return (!PairPredicate.this.test(element1, element2)) || predicate.test(element1, element2);
+			}
+		};
+	}
+
+	default PairPredicate<S, R> inverse()
+	{
+		return new PairPredicate<S, R>()
+		{
+			@Override
+			public final boolean test(final S element1, final R element2)
+			{
+				return PairPredicate.this.test(element2, element1);
+			}
+		};
+	}
 }
