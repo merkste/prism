8a75308b28f62481f7fc08b1929f220b8b346294
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2016-02-17 10:47:36 +0100

    adapt modelviews: Add CallMDP::getNumChoices

--- prism/src/explicit/modelviews/methods/CallMDP.java
+++ prism/src/explicit/modelviews/methods/CallMDP.java
@@ -3,18 +3,59 @@ package explicit.modelviews.methods;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
+import common.functions.AbstractPairMapping;
 import common.functions.AbstractTripleMapping;
+import common.functions.Mapping;
 import common.functions.PairMapping;
+import common.functions.primitive.AbstractMappingFromInteger;
+import common.functions.primitive.MappingFromInteger;
 import common.methods.BinaryMethod;
+import common.methods.UnaryMethod;
 import explicit.MDP;
 
 public class CallMDP
 {
+	private static final MDPGetNumChoices GET_NUM_CHOICES = new MDPGetNumChoices();
 	private static final MDPGetTransitionsIterator GET_TRANSITIONS_ITERATOR = new MDPGetTransitionsIterator();
 
+	public static MDPGetNumChoices getNumChoices()
+	{
+		return GET_NUM_CHOICES;
+	}
 	public static MDPGetTransitionsIterator getTransitionsIterator()
 	{
 		return GET_TRANSITIONS_ITERATOR;
+	}
+
+	public static final class MDPGetNumChoices
+			extends AbstractPairMapping<MDP, Integer, Integer>
+			implements UnaryMethod<MDP, Integer, Integer>
+	{
+		@Override
+		public Integer get(final MDP model, final Integer state)
+		{
+			return model.getNumChoices(state);
+		}
+
+		@Override
+		public MappingFromInteger<Integer> curry(final MDP model)
+		{
+			new AbstractMappingFromInteger<Integer>()
+			{
+				@Override
+				public Integer get(int state)
+				{
+					return model.getNumChoices(state);
+				}
+			};
+			return curry(model);
+		}
+
+		@Override
+		public Mapping<Integer, Integer> on(MDP model)
+		{
+			return curry(model);
+		}
 	}
 
 	public static final class MDPGetTransitionsIterator
