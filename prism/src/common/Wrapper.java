c46661cf451631d1b2aa2c034604be66f43cc39f
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2016-02-17 10:47:10 +0100

    imported patch Wrapper.patch

---
+++ prism/src/common/Wrapper.java
@@ -0,0 +1,61 @@
+//==============================================================================
+//	
+//	Copyright (c) 2015-
+//	Authors:
+//	* Joachim Klein <klein@tcs.inf.tu-dresden.de> (TU Dresden)
+//	
+//------------------------------------------------------------------------------
+//	
+//	This file is part of PRISM.
+//	
+//	PRISM is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//	
+//	PRISM is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//	
+//	You should have received a copy of the GNU General Public License
+//	along with PRISM; if not, write to the Free Software Foundation,
+//	Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//	
+//==============================================================================
+
+package common;
+
+/**
+ * A wrapper around an object of type T. Primary use is to provide a {@code final}
+ * object that can be updated from an anonymous method. 
+ */
+public class Wrapper<T>
+{
+	/** The stored object (or null) */
+	private T t;
+
+	/** Constructor (null) */
+	public Wrapper()
+	{
+		t = null;
+	}
+
+	/** Constructor (t) */
+	public Wrapper(T t)
+	{
+		set(t);
+	}
+
+	/** Set the wrapped object */
+	public void set(T t)
+	{
+		this.t = t;
+	}
+
+	/** Get the wrapped object */
+	public T get()
+	{
+		return t;
+	}
+}
