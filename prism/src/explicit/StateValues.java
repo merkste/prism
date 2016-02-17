e0950e11d6bb1d4e2ab73cd6b4af7655d652facf
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2016-02-17 10:46:06 +0100

    imported patch common-smmd-StateValues.getType.patch

--- prism/src/explicit/StateValues.java
+++ prism/src/explicit/StateValues.java
@@ -1720,4 +1720,9 @@ public class StateValues implements StateVector
 		sv.statesList = statesList;
 		return sv;
 	}
+
+	/** Returns the type of this StateValues object. */
+	public Type getType() {
+		return type;
+	}
 }
