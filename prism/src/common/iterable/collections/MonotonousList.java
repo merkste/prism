244811631c15994a9b39fa4a20fd6678c8385431
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2016-02-17 10:47:40 +0100

    adapt common: implement MonotonousList

---
+++ prism/src/common/iterable/collections/MonotonousList.java
@@ -0,0 +1,39 @@
+package common.iterable.collections;
+
+import java.util.AbstractList;
+
+/**
+ * A monotonous list is an immutable list which elements are identical.
+ *
+ * @author Steffen
+ * @param <T>
+ */
+public class MonotonousList<T> extends AbstractList<T>
+{
+	private final T element;
+	private final int size;
+
+	public MonotonousList(final T element, final int size)
+	{
+		if (size < 0) {
+			throw new IllegalArgumentException("positive list size expected");
+		}
+		this.element = element;
+		this.size = size;
+	}
+
+	@Override
+	public T get(final int index)
+	{
+		if (0 <= index && index < size) {
+			return element;
+		}
+		throw new IndexOutOfBoundsException();
+	}
+
+	@Override
+	public int size()
+	{
+		return size;
+	}
+}
