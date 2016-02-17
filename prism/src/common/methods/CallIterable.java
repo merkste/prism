aec3cdd7a52ab450e6593d9e7a99520cb75f9160
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2016-02-17 10:47:27 +0100

    adapt common: Fix generic type

--- prism/src/common/methods/CallIterable.java
+++ prism/src/common/methods/CallIterable.java
@@ -9,13 +9,13 @@ public class CallIterable
 	private static final Mapping ITERATOR = new Iterator();
 
 	@SuppressWarnings("unchecked")
-	public static <T> Mapping<Iterable<? extends T>, java.util.Iterator<? extends T>> iterator()
+	public static <T> Mapping<Iterable<? extends T>, java.util.Iterator<T>> iterator()
 	{
-		return (Mapping<Iterable<? extends T>, java.util.Iterator<? extends T>>) ITERATOR;
+		return (Mapping<Iterable<? extends T>, java.util.Iterator<T>>) ITERATOR;
 	}
 
 	@SuppressWarnings("rawtypes")
-	public static final class Iterator extends AbstractMapping<Iterable, java.util.Iterator>implements Method<Iterable, java.util.Iterator>
+	public static final class Iterator extends AbstractMapping<Iterable, java.util.Iterator> implements Method<Iterable, java.util.Iterator>
 	{
 		@Override
 		public java.util.Iterator on(final Iterable iterable)
