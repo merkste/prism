0fa593d82db5009e8e21554e2ab2226fe58211db
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2016-02-19 19:30:18 +0100

    Supplement PrimitiveIterable with isEmpty and forEach

--- prism/src/common/iterable/PrimitiveIterable.java
+++ prism/src/common/iterable/PrimitiveIterable.java
@@ -5,4 +5,14 @@ import java.util.PrimitiveIterator;
 public interface PrimitiveIterable<T, T_CONS>
 {
 	public PrimitiveIterator<T, T_CONS> iterator();
+
+	default boolean isEmpty()
+	{
+		return !iterator().hasNext();
+	}
+
+	default void forEach(T_CONS action)
+	{
+		iterator().forEachRemaining(action);
+	}
 }
