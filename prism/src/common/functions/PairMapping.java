df2e3ef7eb1726b7abf5ac29328861fac0403806
Marcus Daum <mdaum@tcs.inf.tu-dresden.de>
2016-02-23 14:25:37 +0100

    common: PairMapping

--- prism/src/common/functions/PairMapping.java
+++ prism/src/common/functions/PairMapping.java
@@ -1,10 +1,19 @@
 package common.functions;
 
+import java.util.Objects;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 @FunctionalInterface
 public interface PairMapping<R, S, T> extends BiFunction<R, S, T>
 {
+	@Override
+	default <V> PairMapping<R, S, V> andThen(Function<? super T, ? extends V> after)
+	{
+		Objects.requireNonNull(after);
+		return (R r, S s) -> after.apply(apply(r, s));
+	}
+
 	default Mapping<S, T> curry(R element1)
 	{
 		return element2 -> apply(element1, element2);
