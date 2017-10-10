96a44506003ea3c93fb652e96a2b2d9b085e5fbb
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2017-10-10 08:25:00 +0200

    Support time-bounded conditions in CTMCs (symbolic)

--- prism/src/prism/conditional/ConditionalMCModelChecker.java
+++ prism/src/prism/conditional/ConditionalMCModelChecker.java
@@ -7,11 +7,15 @@ import explicit.conditional.transformer.MdpTransformerType;
 import jdd.JDD;
 import jdd.JDDNode;
 import parser.ast.Expression;
+import parser.ast.ExpressionBinaryOp;
 import parser.ast.ExpressionConditional;
+import parser.ast.ExpressionProb;
+import parser.ast.RelOp;
 import prism.ModelChecker;
 import prism.ModelExpressionTransformation;
 import prism.Prism;
 import prism.PrismException;
+import prism.PrismNotSupportedException;
 import prism.PrismSettings;
 import prism.ProbModel;
 import prism.ProbModelChecker;
@@ -30,12 +34,8 @@ public abstract class ConditionalMCModelChecker<M extends ProbModel, C extends P
 	}
 
 	@Override
-	public StateValues checkExpression(final M model, final ExpressionConditional expression, JDDNode statesOfInterest) throws PrismException
+	public StateValues checkExpression(M model, ExpressionConditional expression, JDDNode statesOfInterest) throws PrismException
 	{
-		if ((model instanceof StochModel) && !(modelChecker instanceof StochModelChecker)) {
-			throw new PrismException("Cannot model check model type " + model.getModelType());
-		}
-	
 		final NewConditionalTransformer.MC<M,C> transformer = selectModelTransformer(model, expression);
 		final ModelExpressionTransformation<M, ? extends M> transformation = transformModel(transformer, model, expression, statesOfInterest);
 		final StateValues resultTransformed = checkExpressionTransformedModel(transformation, expression);
@@ -89,6 +89,65 @@ public abstract class ConditionalMCModelChecker<M extends ProbModel, C extends P
 		public CTMC(Prism prism, StochModelChecker modelChecker)
 		{
 			super(prism, modelChecker);
+		}
+
+		@Override
+		public StateValues checkExpression(StochModel model, ExpressionConditional expression, JDDNode statesOfInterest) throws PrismException
+		{
+			if (! Expression.containsTemporalTimeBounds(expression.getCondition())) {
+				return super.checkExpression(model, expression, statesOfInterest);
+			}
+			if (!(expression.getObjective() instanceof ExpressionProb)) {
+				throw new PrismException("Cannot model check " + expression);
+			}
+			ExpressionProb objective = (ExpressionProb) expression.getObjective();
+			if (Expression.containsTemporalTimeBounds(objective.getExpression())) {
+				throw new PrismException("Cannot model check " + expression);
+			}
+			Expression transformed = transformExpression(expression);
+			prism.getLog().println();
+			prism.getLog().println("Condition contains time bounds, trying Bayes' rule.");
+			prism.getLog().println("Checking transformed expression: " + transformed);
+			return modelChecker.checkExpression(transformed, statesOfInterest);
+		}
+
+		protected Expression transformExpression(ExpressionConditional expression)
+				throws PrismNotSupportedException
+		{
+			ExpressionProb objective = (ExpressionProb) expression.getObjective();
+			Expression condition     = expression.getCondition();
+
+			// Bayes' rule: P(A|B) = P(B|A) * P(A) / P(B)
+			String computeValues        = RelOp.COMPUTE_VALUES.toString();
+			ExpressionProb newObjective = new ExpressionProb(condition.deepCopy(), computeValues, null);
+			Expression newCondition     = objective.getExpression().deepCopy();
+			ExpressionConditional pAB   = new ExpressionConditional(newObjective, newCondition);
+			ExpressionProb pA           = new ExpressionProb(objective.getExpression().deepCopy(), computeValues, null);
+			ExpressionProb pB           = new ExpressionProb(condition.deepCopy(), computeValues, null);
+			ExpressionBinaryOp fraction = Expression.Divide(Expression.Times(pAB, pA), pB);
+
+			// translate bounds if necessary
+			if (objective.getBound() == null) {
+				return fraction;
+			}
+			int binOp = convertToBinaryOp(objective.getRelOp());
+			return new ExpressionBinaryOp(binOp, Expression.Parenth(fraction), objective.getBound().deepCopy());
+		}
+
+		protected int convertToBinaryOp(RelOp relop) throws PrismNotSupportedException
+		{
+			switch (relop) {
+			case GT:
+				return ExpressionBinaryOp.GT;
+			case GEQ:
+				return ExpressionBinaryOp.GE;
+			case LT:
+				return ExpressionBinaryOp.LT;
+			case LEQ:
+				return ExpressionBinaryOp.LE;
+			default:
+				throw new PrismNotSupportedException("Unsupported comparison operator: " + relop);
+			}
 		}
 
 		@Override
