152501afee7c7e1add3696f8e9f86e23e677b64c
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2020-09-10 10:29:23 +0200

    Optimize FilteringIterator: reduce

--- prism/src/common/iterable/FilteringIterator.java
+++ prism/src/common/iterable/FilteringIterator.java
@@ -35,10 +35,20 @@ import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.OptionalLong;
 import java.util.PrimitiveIterator;
+import java.util.function.BiFunction;
+import java.util.function.DoubleBinaryOperator;
 import java.util.function.DoublePredicate;
+import java.util.function.IntBinaryOperator;
 import java.util.function.IntPredicate;
+import java.util.function.LongBinaryOperator;
 import java.util.function.LongPredicate;
 import java.util.function.Predicate;
+
+import common.functions.ObjDoubleFunction;
+import common.functions.ObjIntFunction;
+import common.functions.ObjLongFunction;
+import common.functions.primitive.DoubleIntOperator;
+import common.functions.primitive.LongIntOperator;
 
 /**
  * Abstract base class implementing an Iterator that filters elements by a predicate.
@@ -185,6 +195,30 @@ public abstract class FilteringIterator<E, I extends Iterator<E>> implements Fun
 		}
 
 		@Override
+		public <T> T reduce(T identity, BiFunction<T, ? super E, T> accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			T result = accumulator.apply(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalIterator) {
+				result = ((FunctionalIterator<E>) iterator).reduce(result, (r, e) -> filter.test(e) ? accumulator.apply(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.next();
+					if (filter.test(next)) {
+						result = accumulator.apply(result, next);
+					}
+				}
+			}
+			release();
+			return result;
+		}
+
+		@Override
 		public void release()
 		{
 			super.release();
@@ -302,6 +336,54 @@ public abstract class FilteringIterator<E, I extends Iterator<E>> implements Fun
 			}
 			release();
 			return OptionalDouble.empty();
+		}
+
+		@Override
+		public <T> T reduce(T identity, ObjDoubleFunction<T, T> accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			T result = accumulator.apply(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfDouble) iterator).reduce(result, (T r, double e) -> filter.test(e) ? accumulator.apply(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextDouble();
+					if (filter.test(next)) {
+						result = accumulator.apply(result, next);
+					}
+				}
+			}
+			release();
+			return result;
+		}
+
+		@Override
+		public double reduce(double identity, DoubleBinaryOperator accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			double result = accumulator.applyAsDouble(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfDouble) iterator).reduce(result, (r, e) -> filter.test(e) ? accumulator.applyAsDouble(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextDouble();
+					if (filter.test(next)) {
+						result = accumulator.applyAsDouble(result, next);
+					}
+				}
+			}
+			release();
+			return result;
 		}
 
 		@Override
@@ -425,6 +507,103 @@ public abstract class FilteringIterator<E, I extends Iterator<E>> implements Fun
 		}
 
 		@Override
+		public <T> T reduce(T identity, ObjIntFunction<T, T> accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			T result = accumulator.apply(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfInt) iterator).reduce(result, (T r, int e) -> filter.test(e) ? accumulator.apply(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextInt();
+					if (filter.test(next)) {
+						result = accumulator.apply(result, next);
+					}
+				}
+			}
+			release();
+			return result;
+		}
+
+		@Override
+		public double reduce(double identity, DoubleIntOperator accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			double result = accumulator.applyAsDouble(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfInt) iterator).reduce(result, (double r, int e) -> filter.test(e) ? accumulator.applyAsDouble(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextInt();
+					if (filter.test(next)) {
+						result = accumulator.applyAsDouble(result, next);
+					}
+				}
+			}
+			release();
+			return result;
+		}
+
+
+		@Override
+		public int reduce(int identity, IntBinaryOperator accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			int result = accumulator.applyAsInt(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfInt) iterator).reduce(result, (int r, int e) -> filter.test(e) ? accumulator.applyAsInt(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextInt();
+					if (filter.test(next)) {
+						result = accumulator.applyAsInt(result, next);
+					}
+				}
+			}
+			release();
+			return result;
+		}
+
+		@Override
+		public long reduce(long identity, LongIntOperator accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			long result = accumulator.applyAsLong(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfInt) iterator).reduce(result, (long r, int e) -> filter.test(e) ? accumulator.applyAsLong(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextInt();
+					if (filter.test(next)) {
+						result = accumulator.applyAsLong(result, next);
+					}
+				}
+			}
+			release();
+			return result;
+		}
+
+		@Override
 		public void release()
 		{
 			super.release();
@@ -541,6 +720,54 @@ public abstract class FilteringIterator<E, I extends Iterator<E>> implements Fun
 			}
 			release();
 			return OptionalLong.empty();
+		}
+
+		@Override
+		public <T> T reduce(T identity, ObjLongFunction<T, T> accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			T result = accumulator.apply(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfLong) iterator).reduce(result, (T r, long e) -> filter.test(e) ? accumulator.apply(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextLong();
+					if (filter.test(next)) {
+						result = accumulator.apply(result, next);
+					}
+				}
+			}
+			release();
+			return result;
+		}
+
+		@Override
+		public long reduce(long identity, LongBinaryOperator accumulator)
+		{
+			Objects.requireNonNull(accumulator);
+			if (!hasNext) {
+				return identity;
+			}
+			// consume current element
+			long result = accumulator.applyAsLong(identity, next);
+			// consume remaining elements
+			if (iterator instanceof FunctionalPrimitiveIterator) {
+				result = ((FunctionalPrimitiveIterator.OfLong) iterator).reduce(result, (r, e) -> filter.test(e) ? accumulator.applyAsLong(r, e) : r);
+			} else {
+				while (iterator.hasNext()) {
+					next = iterator.nextLong();
+					if (filter.test(next)) {
+						result = accumulator.applyAsLong(result, next);
+					}
+				}
+			}
+			release();
+			return result;
 		}
 
 		@Override
